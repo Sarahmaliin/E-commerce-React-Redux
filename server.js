7151d0e2d51e5d22c4ce55cd0b5e138e9e0b95a3
Sarahmaliin <sarahmalinkarlsson@gmail.com>
2021-04-13 11:48:18 +0200

    products backend

---
+++ server.js
@@ -0,0 +1,42 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const mongoose = require('mongoose')
+const shortid = require('shortid')
+
+const app = express();
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost/react-shopping-cart-db', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+})
+
+const Product = mongoose.model('products', new mongoose.Schema({ //when product is created a new id from shortid will be created and set to the id.
+    id : {type: String, default: shortid.generate},
+    title: String,
+    description: String,
+    image: String,
+    price: Number,
+    availableSizes: [String],
+}))
+
+app.get('/api/products', async (req, res) =>{
+    const products = await Product.find({}) //return all products
+    res.send(products)
+})
+
+app.post("/api/products", async (req, res) => {
+    const newProduct = new Product(req.body);
+    const savedProduct = await newProduct.save()
+    res.send(savedProduct)
+})
+
+app.delete('/api/products/:id', async(req, res)=>{
+    const deletedProduct = await Product.findByIdAndDelete(req.params.id);
+    res.send(deletedProduct)
+})
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log('server running at 5000!'))
+
